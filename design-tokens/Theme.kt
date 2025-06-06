a6fde58223485b5fea750adb60e549bace399c13
David Kososhvili <139062850+kosashvili@users.noreply.github.com>
2025-06-06 19:02:43 +0400

    Add design-tokens/Theme.kt

---
+++ design-tokens/Theme.kt
@@ -0,0 +1,42 @@
+package com.example.designsystem.theme
+
+import androidx.compose.material.Colors
+import androidx.compose.ui.graphics.Color
+
+
+val LightColorPalette = Colors(
+    background = Background,
+    backgroundSubdued = BackgroundSubdued,
+    backgroundElevated = BackgroundElevated,
+    backgroundInverted = BackgroundInverted,
+    backgroundInteractive = BackgroundInteractive,
+    backgroundDisabled = BackgroundDisabled,
+    backgroundSuccess = BackgroundSuccess,
+    backgroundWarning = BackgroundWarning,
+    backgroundError = BackgroundError,
+    backgroundInfo = BackgroundInfo,
+    text = Text,
+    textMuted = TextMuted,
+    textSubtle = TextSubtle,
+    textInverted = TextInverted,
+    textOnPrimary = TextOnPrimary,
+    textOnSecondary = TextOnSecondary,
+    textOnSuccess = TextOnSuccess,
+    textOnWarning = TextOnWarning,
+    textOnError = TextOnError,
+    textOnInfo = TextOnInfo,
+    border = Border,
+    borderSubtle = BorderSubtle,
+    borderStrong = BorderStrong,
+    borderDisabled = BorderDisabled,
+    borderSuccess = BorderSuccess,
+    borderWarning = BorderWarning,
+    borderError = BorderError,
+    borderInfo = BorderInfo,
+    surface = Surface,
+    surfaceMuted = SurfaceMuted,
+    surfaceElevated = SurfaceElevated,
+    surfaceOverlay = SurfaceOverlay,
+    surfaceDisabled = SurfaceDisabled,
+    surfaceContrast = SurfaceContrast,
+)
